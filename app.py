07f49d798810b1b08a4a8167e8f14a04e6b46f89
kgiannios <49027457+katyadimova@users.noreply.github.com>
2022-03-08 18:50:05 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import json
+import chat_utils
+from annotated_text import annotated_text
+
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text
+def message(msg,user, order):
+    col1, col12,col2,col3 = st.columns([2,6, 1,8])
+    if user == 'bot':
+        with col2:
+            st.image("images/sea_slug.png", width =50)
+        with col3:
+            if order == 1:
+                annotated_text(("", msg , "#8ef"))
+            else:
+                st.write(msg)
+    else:
+        with col1:
+            st.markdown(" You :smile: "+": ")
+        with col12:
+            if order == 1:
+                annotated_text(("", msg, "#8ef"))
+            else:
+                st.markdown(msg)
+
+st.title('PyTorch Chat Bot')
+
+bot = st.sidebar.selectbox(
+    'Select a bot',
+    ('sea slug', 'cat')
+)
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+
+model_path = "data.pth"
+model, all_words, tags = chat_utils.load_model(model_path)
+
+#st.write('### Bot says:')
+
+if bot:
+    message(msg ='### Bot says: I am Sam the sea slug!', user = 'bot', order = 10)
+    #st.write('### Bot says: I am Sam the sea slug!')
+
+    you_say = get_text()
+    if you_say:
+        bot_say = chat_utils.chatting(model,you_say, all_words, tags)
+        st.session_state.past.append(you_say)
+        st.session_state.generated.append(bot_say)
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated']) - 1, -1, -1):
+            #if i == len(st.session_state['generated'])-1:
+            #    msg = annotated_text(("",st.session_state["past"][i],"#8ef"))
+            #else:
+            #    msg = st.session_state["past"][i]
+            #message(msg, user='you', len(st.session_state['generated'])-i)
+            message(st.session_state["past"][i],user = 'you', order =len(st.session_state['generated']) - i)
+            message(st.session_state["generated"][i], user='bot', order =len(st.session_state['generated']) - i)
---
+++ chat_utils.py
@@ -0,0 +1,73 @@
+#import argparse
+#import os
+#import sys
+#import time
+#import re
+import json
+import random
+
+#import numpy as np
+import torch
+#from torch.optim import Adam
+#from torch.utils.data import DataLoader
+#from torchvision import datasets
+#from torchvision import transforms
+import torch.onnx
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+#import utils
+#from transformer_net import TransformerNet
+#from vgg import Vgg16
+import streamlit as st
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+@st.cache
+def load_model(model_path):
+    print('load model')
+
+    #FILE = "data.pth"
+    data = torch.load(model_path)
+
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data["model_state"]
+
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
+    return model, all_words, tags
+
+
+@st.cache
+def chatting(model, sentence, all_words, tags):
+
+    with open('intents.json', 'r') as json_data:
+        intents = json.load(json_data)
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+    with torch.no_grad():
+        output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                output = random.choice(intent['responses'])
+    else:
+        output = "I do not understand..."
+
+    return output
